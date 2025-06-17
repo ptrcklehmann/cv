5c89c49fdee6b214f71a4181b9e744262f2d738c
Patrick Lehmann <ptrcklehmann.com>
2025-06-17 11:59:25 +0200

    fix: remove unnecessary slash in OpenGraph image URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     url: "https://cv.ptrcklehmann.com",
     images: [
       {
-        url: `${baseSiteUrl}/opengraph-image`,
+        url: `${baseSiteUrl}opengraph-image`,
         width: 1200,
         height: 630,
         alt: `${RESUME_DATA.name}'s profile picture`,
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: `${RESUME_DATA.name} - Resume`,
     description: RESUME_DATA.about,
-    images: [`${baseSiteUrl}/opengraph-image`],
+    images: [`${baseSiteUrl}opengraph-image`],
   },
 };
 
