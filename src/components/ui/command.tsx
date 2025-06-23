ab029f4611d939435865ddc34ecdd8213e90ab8d
Patrick Lehmann <ptrcklehmann.com>
2025-06-23 14:53:21 +0200

    fix: add @radix-ui/react-visually-hidden for improved accessibility in Command component

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { type DialogProps } from "@radix-ui/react-dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
 import * as React from "react";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 
 const Command = React.forwardRef<
@@ -33,6 +34,9 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
                 className="overflow-hidden p-0 shadow-lg"
                 aria-describedby="command-dialog"
             >
+                <VisuallyHidden asChild>
+                    <DialogTitle>Command Dialog</DialogTitle>
+                </VisuallyHidden>
                 <Command className="[&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
                     {children}
                 </Command>
